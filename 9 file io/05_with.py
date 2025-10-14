d05727c7d82b5dbd23f000b69ac591b16d327547
Meenakshi Paul <meenakshipaul379@gmail.com>
2025-10-14 22:18:46 +0530

    Added chapter 9 files

---
+++ 9 file io/05_with.py
@@ -0,0 +1,11 @@
+f = open("file.txt")
+
+print(f.read())
+
+f.close()
+
+# The same can be written using 'with' statement like this:
+with open("file,txt") as f:
+  print(f.read())
+
+# we don't need to explicitly close the file
